1c1ef4619d167e84ddc05e05497e19da788947f1
rezaa98 <45605134+rezaa98@users.noreply.github.com>
2020-10-24 23:01:47 +0700

    instal laravel ui
    instal laravel ui yang cocok dgn laravel 6.x

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::group(
         Route::resource('categories', 'CategoryController');
     }
 );
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
